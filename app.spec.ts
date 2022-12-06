3a5b8634053b33e30ae8f38e149fa3a0b188ab7a
Nabhan Abdullah <nabhan.abdullah@iths.se>
2022-12-06 11:54:04 +0100

    klar med punkt 3

--- app.spec.ts
+++ app.spec.ts
@@ -2,9 +2,11 @@ import {default as request} from "supertest"
 import makeApp from './app'
 
 
-describe('POST /products', ()=>{
+describe('POST, GET /exercise', ()=>{
     const createExercise = jest.fn()
-    const app = makeApp({createExercise})
+    const getAllExercises = jest.fn()
+    const getExerciseById = jest.fn()
+    const app = makeApp({createExercise, getAllExercises, getExerciseById})
 
     it('should return 200 status code', async()=>{
         const response = await request(app).post('/exercise').send({
@@ -14,4 +16,22 @@ describe('POST /products', ()=>{
         })
         expect(response.statusCode).toBe(200)
     })
+
+    it('should return 200 status code', async()=>{
+        const response = await request(app).get('/exercise')
+        expect(response.statusCode).toBe(200)
+    })
+
+    it('should return 404 status code', async()=>{
+        const response = await request(app).get('/exercise/hejhej')
+        expect(response.statusCode).toBe(404)
+    })
+
+    // it('should return 200 status code', async()=>{
+    //     const response = await request(app).get('/exercise/638f1a447d525b4e86ea4c4f')
+    //     expect(response.statusCode).toBe(200)
+    // })
+
+    
+
 } )
